5e12972625204b6e3cdff4984308faf7473379c1
Andy <strawberryit@daum.net>
2020-06-07 23:14:44 +0900

    macos: 맥북 잠자기 상태에서 자동으로 재부팅되는 문제

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# TIL
+> Today I Learned
+오늘 배운 내용을 간결하게 정리하여 모아둔다.
+---
+## Categories
+* [Macos](#macos)
+
+---
+
+## Macos
+* [맥북_잠자기_상태에서_자동으로_재부팅되는_문제.md](macos/맥북_잠자기_상태에서_자동으로_재부팅되는_문제.md)
+
+---
+## Rules
+* Directory and file would be lowercase.
+* Follow GFM(Github Flavored Markdown)
+## Usage
+### Generate `README.md`
+```
+$ python3 build.py
+```
+### Run in Local
+Use [Docker](https://www.docker.com) and [Gollum](https://github.com/gollum/gollum). Details are [here](https://github.com/AWEEKJ/TIL/blob/master/docker/gollum-via-docker.md).
+## Other TIL Collections
+Inspired by
+* [@thoughtbot](https://github.com/thoughtbot/til)
+* [@jbranchaud](https://github.com/jbranchaud/til)
+* [@milooy](https://github.com/milooy/TIL)
+* [@channprj](https://github.com/channprj/TIL)
+
---
+++ build.py
@@ -0,0 +1,70 @@
+#-*- coding: utf-8 -*-
+import re
+import string
+import glob
+import os.path
+import io
+
+
+header = """
+# TIL
+> Today I Learned
+오늘 배운 내용을 간결하게 정리하여 모아둔다.
+---
+"""
+
+footer = """
+---
+## Rules
+* Directory and file would be lowercase.
+* Follow GFM(Github Flavored Markdown)
+## Usage
+### Generate `README.md`
+```
+$ python3 build.py
+```
+### Run in Local
+Use [Docker](https://www.docker.com) and [Gollum](https://github.com/gollum/gollum). Details are [here](https://github.com/AWEEKJ/TIL/blob/master/docker/gollum-via-docker.md).
+## Other TIL Collections
+Inspired by
+* [@thoughtbot](https://github.com/thoughtbot/til)
+* [@jbranchaud](https://github.com/jbranchaud/til)
+* [@milooy](https://github.com/milooy/TIL)
+* [@channprj](https://github.com/channprj/TIL)
+"""
+
+###################################
+
+
+def make_pretty_name(name):
+    pretty_name = re.sub(r'-', ' ', name)
+    return string.capwords(pretty_name)
+
+
+readme = io.open('README.md', 'r+', encoding='utf-8')
+readme.write(header)
+readme.write("## Categories\n")
+
+files = glob.glob('**', recursive=True)
+directories = []
+
+for file in files:
+    if os.path.isdir(file):
+        directories.append(file)
+
+if 'drafts' in directories:
+    directories.remove('drafts')
+
+for directory in directories:
+    readme.write("* [" + directory.capitalize() + "](#" + directory + ")\n")
+
+readme.write("\n---\n")
+
+for directory in directories:
+    readme.write("\n## " + directory.capitalize() + "\n")
+    sub_files = glob.glob(directory + '/*.md')
+    for sub_file in sub_files:
+        readme.write("* [" + make_pretty_name(os.path.basename(sub_file)) + "](" + sub_file + ")\n")
+
+readme.write(footer)
+readme.close
